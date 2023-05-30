95544835b26064b82c644175abf7eb71c4b9f6c0
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-05-30 16:33:28 +0530

    add

--- tasks/task1.c
+++ tasks/task1.c
@@ -3,7 +3,6 @@ int main(){
     int a=23,b=56;
     
     a=b; b=a;
-printf("value of a is %d, and valure of b is %d",a,b);
-
+    printf("value of a is %d, and valure of b is %d",a,b);
     return 0;
 }
