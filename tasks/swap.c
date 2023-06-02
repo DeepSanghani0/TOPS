be1966e47458d596189734c098583afadb05fde8
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-06-02 22:18:22 +0530

    add

---
+++ tasks/swap.c
@@ -0,0 +1,13 @@
+//write a program to swap value of two variable 
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("enter the value of a,b");
+    scanf("%d%d",&a,&b);
+    printf("before swaping value of a = %d & b = %d \n",a,b);
+    a=b-a;
+    b=b-a;
+    a=a+b;
+    printf("after swaping value of a = %d & b = %d",a,b);
+    return 0;
+}
