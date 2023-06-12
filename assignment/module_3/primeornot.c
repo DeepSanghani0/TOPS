9778133ad218763afc0399b46e94f69d98569443
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-06-12 14:39:41 +0530

    add

--- assignment/module_3/primeornot.c
+++ assignment/module_3/primeornot.c
@@ -1,8 +1,25 @@
+//write a program to check given number is prime or not.
 #include<stdio.h>
-int main(){
+int main()
+{
+    int n,c=0,i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
     
-
-
-
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            c++;
+        }
+    }
+    if (c == 2)
+    {
+        printf("given number is prime");
+    }
+    else
+    {
+        printf("given number is not prime");
+    }
     return 0;
 }
