e6df13d7da082f4c2ea4642e4c12b888234c7c16
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-06-12 22:15:04 +0530

    add

--- assignment/module_3/result.c
+++ assignment/module_3/result.c
@@ -21,28 +21,35 @@ int main(){
         scanf("%d",&sanskrit);
             float sum=maths+ science+ sst+ english+ gujarati+sanskrit;
             printf("sum of marks of ALL Subject is %f (out of 600)\n",sum);
-    float percentage= sum/6;
-    printf("you got %f percentage\n",percentage);
-                if (percentage>75)
-                {
-                    printf("congratulation you got Distinction");
-                }
-                else if (percentage> 60 && percentage<= 75)
-                {
-                    printf("First Class");
-                }
-                else if (percentage > 50 && percentage <= 60)
-                {
-                    printf("Second Class");
-                }
-                else if (percentage > 35 && percentage <= 50)
-                {
-                    printf("Pass Class");
-                }
-                else
-                {
-                    printf("FAIL");
-                }
-                
+    
+               if (maths||science||sst||english||gujarati||sanskrit <35)
+               {
+                printf("fail");
+               }
+               else
+               {
+                    float percentage= sum/6;
+                    printf("you got %f percentage\n",percentage);
+                    if (percentage>75)
+                    {
+                        printf("congratulation you got Distinction");
+                    }
+                    else if (percentage> 60 && percentage<= 75)
+                    {
+                        printf("First Class");
+                    }
+                    else if (percentage > 50 && percentage <= 60)
+                    {
+                        printf("Second Class");
+                    }
+                    else if (percentage > 35 && percentage <= 50)
+                    {
+                        printf("Pass Class");
+                    }
+                    else
+                    {
+                        printf("FAIL");
+                    }
+               }
   return 0;
 }
