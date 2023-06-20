b3c5288791f2a878a2a161ee49972da7cd391702
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-06-20 16:31:49 +0530

    add

---
+++ assignment/module_3/pyramid.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int i,j,rows,m;
+    printf("enter the nuber of raws: ");
+    scanf("%d",&rows);
+    for (int i = 1; i <= rows; i++)
+    {   
+        m=i-1;
+        for (int k = 1; k < 6-i; k++)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d",j);
+        }
+        
+
+        for (int l = 1; l <= i-1; l++)
+        {
+            printf("%d",m);
+            m--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
