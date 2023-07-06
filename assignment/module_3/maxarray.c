210e03233e58a1f9c57037fa77e84d050a803e2d
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-06 16:14:48 +0530

    add

---
+++ assignment/module_3/maxarray.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int i;
+    int array[10];
+    for (int i = 0; i < 11; i++)
+    {
+        printf("enter array value array[%d]",array[i]);
+        scanf("%d",&array[i]);
+    }
+    for (int j = 0; j < 11; j++)
+    {
+        printf("%d",array[i]);
+    }
+    
+    
+    return 0;
+}
