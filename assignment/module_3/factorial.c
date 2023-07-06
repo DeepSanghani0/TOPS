0a70b3794aebd6195071d46536d546c664abbea8
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-06 08:31:50 +0530

    add

---
+++ assignment/module_3/factorial.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fect(int no);
+
+int main()
+{   
+    int no;
+    printf("enter the number: ");
+    scanf("%d",&no);
+    printf("%d",fect(no));
+    return 0;
+}
+
+int fect(int no)
+{
+    int a;
+    for (int i = no-1; i >= 1; i--)
+    {
+        no *= i;
+    }
+    return no;
+}
--- assignment/module_3/summation.c
+++ assignment/module_3/summation.c
@@ -5,10 +5,10 @@ int main(){
     int n,a,b,c,d;
     printf("enter the value of n(4 digits): ");
     scanf("%d",&n);
-a=(n/1000);
-b=((n/100)-(a*10));
-c=((n/10)-((a*100)+(b*10)));
-d=(n-((a*1000)+(b*100)+(c*10)));
+    a=(n/1000);
+    b=((n/100)-(a*10));
+    c=((n/10)-((a*100)+(b*10)));
+    d=(n-((a*1000)+(b*100)+(c*10)));
 printf("sum of digits of %d is %d",n,a+b+c+d);
     return 0;
 }
