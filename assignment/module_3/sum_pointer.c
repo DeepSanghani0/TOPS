a06da0311c67f9d43b0ce4ed82acc002de5ea0b4
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-10 10:53:31 +0530

    add

--- assignment/module_3/sum_pointer.c
+++ assignment/module_3/sum_pointer.c
@@ -1,7 +1,9 @@
 //Write program to make an addition of two number using pointer.
 #include<stdio.h>
 int main(){
+
     int a,b,*pointer1,*pointer2,sum;
+
     printf("enter first number: ");
     scanf("%d",&a);
     printf("enter second number: ");
@@ -10,6 +12,7 @@ int main(){
     pointer1=&a;
     pointer2=&b;
     sum = *pointer1 + *pointer2;
+    
     printf("sum of given number is %d",sum);
 
 return 0;
