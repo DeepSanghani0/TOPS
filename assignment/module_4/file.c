93cbfc2c1adaafd3d109fdb9df9b051e804f0e2e
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-21 09:57:17 +0530

    add

---
+++ assignment/module_4/file.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+ int no,i,multiply;
+ char ch;
+int main(){
+    FILE *table;
+   
+    printf("enter no.: ");
+    scanf("%d",&no);
+    for (int i = 1; i <= 10; i++)
+    {
+        multiply=no*i;
+    {
+    table = fopen("table.txt","a");
+    fscanf(table,"%d %d %d",&no,&i,&multiply);
+    fprintf(table,"%d %d %d \n",no,i,multiply);
+    fclose(table);
+    }
+    
+    //read data from file
+    table = fopen("table.txt","r");
+    ch = fgetc(table);
+
+    while (ch != EOF)
+    {
+        ch = fgetc(table);
+        printf("%c",ch);
+    } 
+    fclose(table);
+    }
+    return 0;
+}
