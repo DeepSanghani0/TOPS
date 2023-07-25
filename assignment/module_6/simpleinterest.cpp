caaa4aa1ca429ed900700fabdd45ae34676f44e7
DeepSanghani0 <133763612+DeepSanghani0@users.noreply.github.com>
2023-07-25 11:10:48 +0530

    add

--- assignment/module_6/simpleinterest.cpp
+++ assignment/module_6/simpleinterest.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 class simple
 {
+    int principal,rate,time,interest,SI;
     public: 
     simple()
     {
-        int principal,rate,time,interest,SI;
         cout<<"enter value of principal: ";
         cin>>principal;
         cout<<"enter value of rate of interst: ";
         cin>>rate;
         cout<<"enter time period(in year): ";
         cin>>time;
+    }
+    initialize()
+    {
         SI=((principal*rate*time)/100);
         cout<<"simple interst of given values are : "<<SI<<endl;
         cout<<"total payable amount is "<< SI+principal<<endl; 
